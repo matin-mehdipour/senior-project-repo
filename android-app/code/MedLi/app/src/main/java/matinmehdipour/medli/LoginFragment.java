d10bfca739236bd144f146c9929ff89a670ec766
Matin Mehdipour <matin.mehdipour90@gmail.com>
2015-11-10 22:37:03 -0800

    add more stuff to make login/registration more functional

--- android-app/code/MedLi/app/src/main/java/matinmehdipour/medli/LoginFragment.java
+++ android-app/code/MedLi/app/src/main/java/matinmehdipour/medli/LoginFragment.java
@@ -1,5 +1,6 @@
 package matinmehdipour.medli;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,14 +8,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 /**
  * Created by matt on 11/6/15.
  */
-public class LoginFragment extends Fragment
+public class LoginFragment extends Fragment implements View.OnClickListener
 {
     View v;
     Button registerButton;
+    Button loginButton;
+
+    EditText emailET;
+    EditText passwordET;
 
     LoginActivity loginActivity;
 
@@ -23,6 +33,13 @@ public class LoginFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         v = inflater.inflate(R.layout.account_login_fragment, container, false);
+
+        loginButton = (Button)v.findViewById(R.id.login_button);
+        loginButton.setOnClickListener(this);
+
+        emailET = (EditText)v.findViewById(R.id.email_et_signin);
+        passwordET = (EditText)v.findViewById(R.id.password_et_signin);
+
         return v;
     }
 
@@ -34,13 +51,35 @@ public class LoginFragment extends Fragment
         loginActivity = (LoginActivity)getActivity();
 
         registerButton = (Button)v.findViewById(R.id.register_button);
-        registerButton.setOnClickListener(new View.OnClickListener()
+        registerButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if(v == loginButton)
         {
-            @Override
-            public void onClick(View v)
+            String email = emailET.getText().toString();
+            String password = passwordET.getText().toString();
+
+
+            ParseUser.logInInBackground(email, password, new LogInCallback()
             {
-                loginActivity.switchToRegistration();
-            }
-        });
+                @Override
+                public void done(ParseUser parseUser, ParseException e)
+                {
+                    if(e == null)
+                    {
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }
+                }
+            });
+        }
+        else if(v == registerButton)
+        {
+            loginActivity.switchToRegistration();
+        }
     }
 }
--- android-app/code/MedLi/app/src/main/java/matinmehdipour/medli/RegisterFragment.java
+++ android-app/code/MedLi/app/src/main/java/matinmehdipour/medli/RegisterFragment.java
@@ -6,6 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 /**
  * Created by matt on 11/6/15.
@@ -13,12 +22,108 @@ import android.view.ViewGroup;
 public class RegisterFragment extends Fragment
 {
     View v;
+    Button backToLoginButton;
+
+    LoginActivity loginActivity;
+
+    //editables
+    EditText fullNameET;
+    EditText emailET;
+    EditText passwordET;
+    EditText confirmPwET;
+
+    //spinner
+    Spinner accountTypeSpinner;
+
+    String[] accountTypeArray = {"Consumer", "Publisher"};
+
+    Button registerButton;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         v = inflater.inflate(R.layout.account_register_fragment, container, false);
+
+
+        fullNameET = (EditText)v.findViewById(R.id.full_name_name_et);
+        emailET = (EditText)v.findViewById(R.id.email_et_signin);
+        passwordET = (EditText)v.findViewById(R.id.password_et);
+        confirmPwET = (EditText)v.findViewById(R.id.confirm_password_et);
+
+        accountTypeSpinner = (Spinner)v.findViewById(R.id.account_type_spinner);
+
+
+
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(getActivity(),
+                        android.R.layout.simple_spinner_dropdown_item, accountTypeArray);
+
+        accountTypeSpinner.setAdapter(arrayAdapter);
+
+        registerButton = (Button)v.findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                doRegister();
+
+            }
+        });
+
         return v;
+    }
+
+    private void takeBackToLogin(String email)
+    {
+        loginActivity.switchToLogin();
+    }
+
+    private void doRegister()
+    {
+        String fullName = fullNameET.getText().toString();
+        final String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+        String confirmPW = confirmPwET.getText().toString();
+        String accountType = accountTypeSpinner.getSelectedItem().toString();
+
+        ParseUser parseUser = new ParseUser();
+        parseUser.setUsername(email);
+        parseUser.setEmail(email);
+        parseUser.put("full_name", fullName);
+        parseUser.put("account_type", accountType);
+        parseUser.setPassword(password);
+        parseUser.signUpInBackground(new SignUpCallback()
+        {
+            @Override
+            public void done(ParseException e)
+            {
+                if(e == null)
+                {
+                    Toast.makeText(getActivity(), "Success!", Toast.LENGTH_SHORT).show();
+                    takeBackToLogin(email);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        loginActivity = (LoginActivity)getActivity();
+
+        backToLoginButton = (Button)v.findViewById(R.id.back_to_login_button);
+
+        backToLoginButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                loginActivity.switchToLogin();
+            }
+        });
     }
 }
